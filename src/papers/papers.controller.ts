e3c76021cfefb2f396fec3b7e107b515a11d5488
jungwoo <kjw21ds@naver.com>
2026-05-01 14:02:09 +0900

    페이지네이션 공통 코드 추가

--- src/papers/papers.controller.ts
+++ src/papers/papers.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PapersService } from './papers.service';
+import { GetPapersDto } from './dto/get-papers.dto';
 
 @Controller('papers')
 export class PapersController {
   constructor(private readonly papersService: PapersService) {}
+
+  @Get()
+  getAllPapers(
+    @Query() dto: GetPapersDto,
+  ) {
+    return this.papersService.getAllPapers(dto);
+  }
 }
--- src/papers/papers.service.ts
+++ src/papers/papers.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Papers } from './entities/papers.entity';
+import { Repository } from 'typeorm';
+import { GetPapersDto } from './dto/get-papers.dto';
+import { CommonService } from 'src/common/common.service';
 
 @Injectable()
-export class PapersService {}
+export class PapersService {
+  constructor(
+    @InjectRepository(Papers)
+    private readonly papersRepository: Repository<Papers>,
+    private readonly commonService: CommonService,
+
+  ){}
+  async getAllPapers(dto: GetPapersDto) {
+
+    const {title} = dto;
+
+    const qb = await this.papersRepository.createQueryBuilder('paper')
+
+    if(title){
+      qb.where('paper.title LIKE :title', {title: `%${title}%`});
+    }
+
+    const {nextCursor} = await this.commonService.pagination(qb, dto);
+
+    let [data, count] = await qb.getManyAndCount();
+
+
+    return { 
+      data,
+      nextCursor,
+      count,
+    }; 
+  }
+
+}
