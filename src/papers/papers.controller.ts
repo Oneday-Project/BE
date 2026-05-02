9e8d98a31e94283d839645939ce4ddcaa2e975d3
jungwoo <kjw21ds@naver.com>
2026-05-02 17:01:15 +0900

    author, 분야 테이블 분리 및 getAllPapers() 수정

--- src/papers/papers.controller.ts
+++ src/papers/papers.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PapersService } from './papers.service';
 import { GetPapersDto } from './dto/get-papers.dto';
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { RolesEnum } from 'src/users/const/roles.const';
 
 @Controller('papers')
 export class PapersController {
@@ -11,5 +13,31 @@ export class PapersController {
     @Query() dto: GetPapersDto,
   ) {
     return this.papersService.getAllPapers(dto);
+  }
+
+  // @Get()
+  // getPaperByArxivId(
+  //   @Query('arxivId') arxivId: string, 
+  // ){
+
+  // }
+
+  @Get('category')
+  getAllCategory(){
+    return this.papersService.getAllCategories();
+  }
+
+  @Get('authors')
+  @Roles(RolesEnum.ADMIN)
+  getAllAuthors(){
+    return this.papersService.getAllAuthors();
+  }
+
+  @Post('category')
+  @Roles(RolesEnum.ADMIN)
+  createCategory(
+    @Body('name') name: string,
+  ){
+    return this.papersService.createCategory(name);
   }
 }
