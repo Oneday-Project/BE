e25c286e8be7028bb744e31b2d2768623ff59a62
jungwoo <kjw21ds@naver.com>
2026-05-02 21:35:14 +0900

    주석 수정

--- src/papers/entities/author.entity.ts
+++ src/papers/entities/author.entity.ts
@@ -1,6 +1,4 @@
-// [변경] ManyToMany 역참조를 위해 ManyToMany 데코레이터 추가
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
-// [변경] Papers 순환 참조 방지를 위해 forwardRef 패턴 사용
 import { Papers } from "./papers.entity";
 import { BaseModel } from "src/common/entities/base.entity";
 
@@ -15,7 +13,6 @@ export class Author extends BaseModel {
     @Column({nullable: true})
     authorId?: string;
 
-    // [변경] Papers와의 ManyToMany 역참조 추가
     @ManyToMany(() => Papers, (paper) => paper.authors)
     papers!: Papers[];
 }
