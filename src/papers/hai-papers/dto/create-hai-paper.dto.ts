d1e1e1ff02df4bbdc873d13a66871175b153c2ba
jungwoo <kjw21ds@naver.com>
2026-07-13 17:55:31 +0900

    hai논문 post 코드 수정

--- src/papers/hai-papers/dto/create-hai-paper.dto.ts
+++ src/papers/hai-papers/dto/create-hai-paper.dto.ts
@@ -1,17 +1,31 @@
+import { IsArray, IsOptional, IsString } from 'class-validator';
+
 export class CreateHAIpaperDto {
+    @IsOptional()
+    @IsString()
     doi?: string;
 
+    @IsString()
     title!: string;
 
+    @IsArray()
+    @IsString({ each: true })
     authors!: string[];
 
+    @IsOptional()
+    @IsString()
     academic_advisor?: string;
 
+    @IsOptional()
+    @IsString()
     department?: string;
 
+    @IsString()
     abstract!: string;
 
+    @IsString()
     publishedYear!: string;
-    
+
+    @IsString()
     pdfUrl!: string;
 }
